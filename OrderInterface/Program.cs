81b4ac61e5427a59ecb76730a3bcf5c4a599f1f6
owhiteman <ollie.whiteman@gmail.com>
2021-06-25 12:38:18 +1200

    Initial check-in of module OrderInterface

---
+++ OrderInterface/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OrderInterface
+{
+    public interface IOrder
+    {
+        void GetOrder();
+    }
+
+    public class CustomerOrder : IOrder
+    {
+        public void GetOrder()
+        {
+            Console.WriteLine("Customer Order");
+        }
+    }
+
+    public class PurchaseOrder : IOrder
+    {
+        public void GetOrder()
+        {
+            Console.WriteLine("Purchase Order");
+        }
+    }
+
+    class MainClass
+    {        
+
+        public static void Main()
+        {
+            CustomerOrder customer = new CustomerOrder();
+            PurchaseOrder purchase = new PurchaseOrder();
+
+            customer.GetOrder();
+            purchase.GetOrder();
+        }
+    }
+}
